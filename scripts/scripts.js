44769e2e970e8d561ad09aeb18cffe5f11a1d5fa
Mozaick <mozeselbarbary@developersinstitute.co.nz>
2022-01-04 17:02:29 +1300

    cleaning up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,9 +31,6 @@ let currentPlayer = 0;
 
 let diceTwoSix;
 
-// HTML Document has initial values
-// zero all these values at start
-
 zero();
 
 function zero() {
@@ -46,7 +43,6 @@ function zero() {
 let rollDiceBtn = document.getElementsByClassName('roll-dice')[0];
 
 rollDiceBtn.addEventListener('click', () => {
-  //hide input when game starts
   inputDisplayNone();
   if (gameStarter) {
     let dice = Math.floor(Math.random() * 6) + 1;
@@ -57,14 +53,7 @@ rollDiceBtn.addEventListener('click', () => {
 
     let score = document.getElementById('score-' + currentPlayer);
 
-    console.log(dice, diceTwoSix); // @beginning logs (dice, diceTwoSix logs undefined)
-
-    // i placed diceTwoSix condition here bcz
-    // i want this if condition to have diceTwoSix defined
-    // before i create a new value for dice
-    // which means that if i clicked 'rollDice' btn
-    // and dice becomes 6 the condition is a truthy
-    // if the stored value for diceTwoSix was 6
+    console.log(dice, diceTwoSix);
 
     if (dice === 6 && diceTwoSix === 6) {
       savedScores[currentPlayer] = 0;
@@ -83,19 +72,8 @@ rollDiceBtn.addEventListener('click', () => {
     }
 
     diceTwoSix = dice;
-    // i declared the value of 'diceTwoSix' here to store,
-    // the value of 'dice' before it changes again,
-    // by doing that now i have a value of 'dice' that occurred at the end,
-    // and a value of 'dice' that occurs at the beginning,
-    // for example if value of 'dice' when 'rollDice' btn clicked is 5,
-    // value of 'diceTwoSix' will be 5 and it will store that value in,
-    // 'diceTwoSix' until the next 'rollDice' btn click happens,
-    // so if the next value for 'dice' is 6, and since 'dice' creates random numbers
-    // the value of 'diceTwoSix' will still be 5 until the next btn click occurs
-    // and before that happens 'dice' value is 6 and 'diceTwoSix' value is 5.
-    // check where i put the condition that relates to diceTwoSix
 
-    console.log(diceTwoSix); // @beginning diceTwoSix logs (latest value of dice)
+    console.log(diceTwoSix);
   }
 });
 
@@ -133,7 +111,6 @@ function switchPlayer() {
 let newGameBtn = document.querySelector('.new-game');
 
 newGameBtn.addEventListener('click', () => {
-  //display input to setScore
   inputDisplayOn();
   gameStarter = true;
 
